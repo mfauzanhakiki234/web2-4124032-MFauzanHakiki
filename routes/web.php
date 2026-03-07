45efdacc85e43a7d9eafe162c846704181868463
mfauzan hakiki123 <mfauzanhakiki@saintek.unipdu.ac.id>
2026-03-07 15:34:32 +0700

    Initial commit proyek Laravel - FauzanHakiki

---
+++ routes/web.php
@@ -0,0 +1,16 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+Route::get('/perkenalan', function () {
+
+    return '<h1>Halo! Nama saya Fauzan Hakiki</h1>
+
+            <p>NIM: 4124032 | Prodi: Sistem Informasi</p>
+
+            <p>Saya siap belajar Laravel! 🚀</p>';
+
+});
